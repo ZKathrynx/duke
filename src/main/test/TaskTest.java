cc01c03e1b489e22a043dc069811b1868b882c79
zkathrynx <katherine111433@gmail.com>
2019-08-25 22:18:55 +0800

    added test

---
+++ src/main/test/TaskTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskTest {
+
+    @Test
+    void getTaskName() {
+        Task t = new Task("test");
+        assertEquals(t.getTaskName(),"test");
+    }
+}
