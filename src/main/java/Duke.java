733b8a59f6cac0ee2469f712cd136a35b1df6da3
zkathrynx <katherine111433@gmail.com>
2019-08-25 20:49:35 +0800

    try first commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,36 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("____________________________________________________________");
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        /*Scanner s = new Scanner(System.in);
+        String command = s.nextLine();
+        UserInstr user = new UserInstr(command);
+        while(!user.sayBye()) {
+            user.echo();
+        }*/
+    }
+}
+
+class UserInstr {
+    private String instr;
+    private boolean isBye = false;
+
+    UserInstr (String instr) {
+        this.instr = instr;
+    }
+
+    public boolean sayBye () {
+        return isBye;
+    }
+    public void printLine () {
+        System.out.println("____________________________________________________________");
+    }
+
+    public void echo () {
+        System.out.println(instr);
     }
 }
